e2daf1280d8c8a528d385fadb243b5398f879549
cocoatehcat <cocoa.teh.cat@gmail.com>
2025-11-03 11:15:49 -0500

    End of class commit! Created the Unity project and moved the git ignore around to make sure the only things committed are the important bits & bobs. Added a capsule collider for the player.

---
+++ Butterfly/Assets/Scripts/CapsuleController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/*
+ * 
+ * Created by: Arija Hartel (@cocoatehcat)
+ * Purpose: For allowing the player to be able to move!
+ * 
+ */
+
+public class CapsuleController : MonoBehaviour
+{
+    public InputActionMap player;
+}
