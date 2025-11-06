0245229b80a683ff409764aa75bcaed969ecd91a
cocoatehcat <cocoa.teh.cat@gmail.com>
2025-11-06 11:12:40 -0500

    Made a simple input controller system with a rolling ball. Will obviously change shape but was just messing around with movement.

--- Butterfly/Assets/Scripts/CapsuleController.cs
+++ Butterfly/Assets/Scripts/CapsuleController.cs
@@ -10,5 +10,29 @@ using UnityEngine.InputSystem;
 
 public class CapsuleController : MonoBehaviour
 {
-    public InputActionMap player;
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
+    public float speed = 5;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
 }
